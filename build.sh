467d82a5dc50843d97a185847c2039f44d4cbe70
clashroyalmahdi-pixel <clashroyal.mahdi@gmail.com>
2025-08-19 02:05:49 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 set -o errexit
 pip install -r requirements.txt
 python manage.py migrate
-python manage.py createsuperuser --noinput
+python manage.py createsuperuser --noinput --username=$DJANGO_SUPERUSER_USERNAME --email=$DJANGO_SUPERUSER_EMAIL
 python manage.py collectstatic --noinput
